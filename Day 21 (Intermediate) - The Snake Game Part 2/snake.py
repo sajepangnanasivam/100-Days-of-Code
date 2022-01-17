182744ebdf7ab5f0005e6f032d879d28b34c5cdc
Sajepan Gnanasivam <sajepan.gnanasivam@gmail.com>
2022-01-17 16:13:52 +0100

    Day 21 Done

---
+++ Day 21 (Intermediate) - The Snake Game Part 2/snake.py
@@ -0,0 +1,60 @@
+from turtle import Turtle, Screen
+
+# Creating a constants
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        # TODO:(1) Create Snake body
+        # A turtle has a dimension of 20x20
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    # TODO:(7) Extend the snake everytime it eats
+    def extend(self):
+        # Getting the position of the last segment
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def move(self):
+        # TODO:(2) How to move forwards
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        # Moving the first segments and the rest will follow.
+        self.head.forward(MOVE_DISTANCE)
+
+    # TODO:(3) How to control the snake
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
