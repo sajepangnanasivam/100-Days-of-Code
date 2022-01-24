adc95cf34375015c5ecad67c43866f0a031ed897
Sajepan Gnanasivam <sajepan.gnanasivam@gmail.com>
2022-01-24 20:12:31 +0100

    Day 24 Done

---
+++ Day 24 (Intermediate) - Files, Directories and Paths/(24.0) Snake Game with Highscore/scoreboard.py
@@ -0,0 +1,39 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("courier", 16, "bold")
+
+
+# TODO:(5) Create a scoreboard
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("White")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.color("red")
+    #     self.write(f"GAME OVER!", align=ALIGNMENT, font=FONT)
